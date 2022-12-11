9641d969600368c90360444303a8192997166617
kkmaru <89262302+kmarulab@users.noreply.github.com>
2022-12-11 17:42:44 -0500

    Add files via upload

---
+++ GameOfLife/gLife_backpack.ino
@@ -0,0 +1,192 @@
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
+
+Adafruit_8x8matrix matrix = Adafruit_8x8matrix();
+ volatile int n=0;
+ volatile int gen=0;
+  bool states[6][10][10]={{
+        //octagon
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
+        {0, 0, 0, 1, 0, 0, 1, 0, 0, 0},
+        {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+        {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+        {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+        {0, 0, 0, 1, 0, 0, 1, 0, 0, 0},
+        {0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    },
+    {
+        //mold
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+        {0, 0, 0, 0, 1, 0, 0, 1, 0, 0},
+        {0, 0, 0, 1, 0, 1, 0, 1, 0, 0},
+        {0, 0, 0, 1, 0, 0, 1, 0, 0, 0},
+        {0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    },
+    //state2
+    {
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+      {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+      {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+      {0, 0, 0, 1, 0, 0, 1, 0, 1, 0},
+      {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+      {0, 1, 0, 1, 0, 0, 1, 0, 0, 0},
+      {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+      {0, 0, 0, 1, 0, 0, 0, 0, 1, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+      },
+      //state3
+      {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    },
+     //state4
+     {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 1, 0, 0, 1, 0, 1, 0},
+        {0, 1, 1, 0, 0, 0, 0, 1, 0, 0},
+        {0, 1, 0, 0, 1, 1, 1, 0, 1, 0},
+        {0, 0, 0, 1, 1, 0, 1, 0, 0, 0},
+        {0, 1, 0, 0, 0, 1, 1, 0, 0, 0},
+        {0, 1, 0, 0, 0, 1, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+        {0, 1, 0, 1, 1, 0, 1, 1, 1, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    },
+    //state5
+    {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+        {0, 1, 1, 0, 0, 0, 0, 1, 1, 0},
+        {0, 1, 0, 0, 1, 1, 1, 0, 1, 0},
+        {0, 1, 0, 1, 1, 0, 1, 0, 1, 0},
+        {0, 1, 0, 1, 0, 1, 1, 0, 1, 0},
+        {0, 1, 0, 1, 1, 1, 0, 0, 1, 0},
+        {0, 1, 1, 0, 0, 0, 0, 0, 1, 0},
+        {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    }
+    };
+void setup() {
+  Serial.begin(9600);
+  Serial.println("8x8 LED Matrix Test"); 
+  matrix.begin(0x70);  // pass in the address
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(gen>9){
+    n++;
+    gen=0;
+      }
+      if(n<6){
+      displayGrid(states[n]);
+      evaluateGrid(states[n]);
+      Serial.println("\t\t\t\tNew Generation");
+      Serial.print("Generation: ");
+      Serial.println(gen);
+      Serial.print("State Numnber: ");
+      Serial.println(n);
+      delay(500);
+      matrix.clear();}
+      else{
+        matrix.setTextSize(1);
+  matrix.setTextWrap(false);  // we dont want text to wrap so it scrolls nicely
+  matrix.setTextColor(LED_ON);
+  for (int8_t x=0; x>=-36; x--) {
+    matrix.clear();
+    matrix.setCursor(x,0);
+    matrix.print("The");
+    matrix.writeDisplay();
+    delay(100);
+        }
+  for (int8_t x=7; x>=-36; x--) {
+    matrix.clear();
+    matrix.setCursor(x,0);
+    matrix.print("End!");
+    matrix.writeDisplay();
+    delay(100);
+  }
+}
+
+}
+void displayGrid(bool gridOne[10][10]){
+  for(int m=1;m<9;m++){
+    for(int n=1;n<9;n++){
+          if(gridOne[m][n]==true){
+            //light the respective led
+            matrix.drawPixel(m-1, n-1, 1);
+            matrix.writeDisplay();
+            //Serial.print(" 0 ");
+            if(n==8){
+              //Serial.print("\n");
+              }           
+          }
+          else{
+            //do nothing
+            //Serial.print(" . ");
+            if(n==8){
+              //Serial.print("\n");
+            }
+         }        
+      }
+    }
+  gen++;
+  }
+
+ void copyGrid(bool gridOne[10][10], bool gridTwo[10][10]){
+    for(int m=0;m<9;m++){
+      for(int n=0; n<9;n++){
+          gridTwo[m][n]=gridOne[m][n];
+        }
+      }
+  }
+
+void evaluateGrid(bool gridOne[10][10]){
+    bool gridTwo[10][10]={};
+    copyGrid(gridOne,gridTwo);
+    //first two outer loops to access individual cells
+    //two inner loops to compare the state
+    for(int j=1;j<9;j++){
+      for(int k=1;k<9;k++){
+        int neighbours=0;
+        
+        for(int a=-1;a<2;a++){
+          for(int b=-1;b<2;b++){
+            if(!(a==0 && b==0)){
+              if(gridTwo[j+a][k+b]){
+                ++neighbours;
+                }
+              }
+            }
+          }
+
+          if(neighbours<2){
+            gridOne[j][k]=false;
+            }
+          else if(neighbours==3){
+            gridOne[j][k]= true;
+            }
+           else if(neighbours>3){
+            gridOne[j][k]= false;
+            }
+        }
+      }
+    }
